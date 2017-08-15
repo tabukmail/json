664bd731078e34b61d94ecd579b5d7c789317667
tabuk <tabukmail@gmail.com>
2017-08-15 15:31:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 var jcontent = "https://api.cryptowat.ch/markets/prices"
 var output = document.getElementById('output');
-output.innerHTML = jcontent.result;
+output.innerHTML = "test"
+  
+  //jcontent.result;
