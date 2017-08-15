390531dcef05b58094278c454147138d9045b7bf
tabuk <tabukmail@gmail.com>
2017-08-15 14:55:20 +0300

    Update my_script.js

--- my_script.js
+++ my_script.js
@@ -1,6 +1,23 @@
-$(document).ready( function() {
- $.getJSON("https://api.cryptowat.ch/markets/prices", function(data){
-       $.each(data.result, function(){
-	         $("ul").append("<li>bitfinex:btcusd: "+this['bitfinex:btcusd']+"</li> <br/>");
-	   });
- });
+// Examples use USGS Earthquake API:
+//   https://earthquake.usgs.gov/fdsnws/event/1/#methods
+var earthquakes;
+function preload() {
+  // Get the most recent earthquake in the database
+  var url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?' +
+    'format=geojson&limit=1&orderby=time';
+  earthquakes = loadJSON(url);
+}
+
+function setup() {
+  noLoop();
+}
+
+function draw() {
+  background(200);
+  // Get the magnitude and name of the earthquake out of the loaded JSON
+  var earthquakeMag = earthquakes.features[0].properties.mag;
+  var earthquakeName = earthquakes.features[0].properties.place;
+  ellipse(width/2, height/2, earthquakeMag * 10, earthquakeMag * 10);
+  textAlign(CENTER);
+  text(earthquakeName, 0, height - 30, width, 30);
+}
