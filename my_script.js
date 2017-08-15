5bd0ca5b132a0f6843efbcd3cffcc0533faf31c3
tabuk <tabukmail@gmail.com>
2017-08-15 14:28:26 +0300

    Update my_script.js

--- my_script.js
+++ my_script.js
@@ -1,7 +1,6 @@
 $(document).ready( function() {
  $.getJSON("https://api.cryptowat.ch/markets/prices", function(data){
        $.each(data.result, function(){
-	         $("ul").append("<li>bitfinex:btcusd: "+this['bitfinex:btcusd']+"</li>
-                                                               <br />");
+	         $("ul").append("<li>bitfinex:btcusd: "+this['bitfinex:btcusd']+"</li> <br/>");
 	   });
  });
